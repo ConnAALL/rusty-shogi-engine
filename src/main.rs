f736b9d9e8fdb64baeab867ef5717b39a5ce763c
russellkosovsky <russellkosovsky@gmail.com>
2023-06-13 15:07:58 -0400

    testing library

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,29 @@
 // Russell Kosovsky
+
 mod view;
-use shogi::Position;
-use shogi::bitboard::Factory as BBFactory;
 use shogi_core::PartialPosition;
 use shogi_legality_lite::all_legal_moves_partial;
-    
+
+
 fn main() {
-    BBFactory::init();
-    let mut pos = Position::new();
-
-    // Position can be set from the SFEN formatted string.
-    pos.set_sfen("lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b - 1").unwrap();
-    
-    println!("STARTING BOARD:");
-    println!("{}", pos.to_sfen());
-    let sfen = pos.to_sfen();
-    view::display_sfen(&sfen);
-
     let startpos = PartialPosition::startpos();
 
-    let moves = all_legal_moves_partial(&startpos);
+    let stri = startpos.to_sfen_owned();
+    println!("STARTING BOARD");
+    println!("{}", stri);
+    view::display_sfen(&stri);
+    
 
+    let moves = all_legal_moves_partial(&startpos);
     println!("All Possible Moves:");
     for move_item in moves {
         println!("{:?}", move_item);
     }
+    
 }
+
+
+
+
+
+
