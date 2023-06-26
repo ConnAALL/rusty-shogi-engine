b32f7950864c99db0013132784eef1b2fe0540a9
russellkosovsky <russellkosovsky@gmail.com>
2023-06-26 13:58:50 -0400

    added pst function with static pst values and it. its stored as a hashmap and you can access the pst values from a given piece and coord

--- src/main.rs
+++ src/main.rs
@@ -89,4 +89,6 @@ fn main() {
     let sfen = "lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL w - 1";
     let sfen_tst = eval::pst_parse(sfen);
     println!("{:?}", sfen_tst);
+
+    eval::pst();
 }
