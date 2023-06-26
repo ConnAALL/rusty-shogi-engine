5b8aec5fd2ed91551bf59ebaca9c29419ba3a371
russellkosovsky <russellkosovsky@gmail.com>
2023-06-26 13:09:09 -0400

    reverted sfen back to starting pos

--- src/main.rs
+++ src/main.rs
@@ -86,7 +86,7 @@ fn main() {
     //manual_test();
     //partial_pos_test();
     
-    let sfen = "+l+n+sgkgsnl/1+r5+b1/+ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL w - 1";
+    let sfen = "lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL w - 1";
     let sfen_tst = eval::pst_parse(sfen);
     println!("{:?}", sfen_tst);
 }
