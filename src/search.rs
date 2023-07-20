66025a306d76c6f21d41bc537d6aac5d7b5fd411
russellkosovsky <russellkosovsky@gmail.com>
2023-07-20 10:31:09 -0400

    reformat

--- src/search.rs
+++ src/search.rs
@@ -8,82 +8,6 @@ use shogi_legality_lite::all_legal_moves_partial;
 use std::collections::HashSet;
 use std::collections::VecDeque;
 
-pub fn search(sfen: &str, depth: u32, current_depth: u32) -> Vec<String> {
-    let positions = sfen::sfen_parse(sfen);// creates list of board squares and the pieces on them (if there are any)
-    let mut pos = sfen::generate_pos(positions); // creates a "partial position" out of it
-    pos.side_to_move_set(sfen::get_color(sfen));
-
-    if current_depth == depth {
-        return vec![sfen.to_string()]; // Return the SFEN string as a single-element vector
-    }
-
-    let next_moves = all_legal_moves_partial(&pos);
-    let mut sfen_list = Vec::new();
-
-    for move_item in next_moves {
-        let mut temp_pos = pos.clone();
-        temp_pos.make_move(move_item);
-        let sfen = temp_pos.to_sfen_owned();
-
-        let deeper_moves = search(&sfen, depth, current_depth + 1);
-        sfen_list.extend(deeper_moves);
-    }
-
-    sfen_list
-}
-
-
-pub fn has_duplicates<T: std::cmp::Eq + std::hash::Hash>(vec: &[T]) -> bool {
-    let set: HashSet<_> = vec.iter().collect(); // Create a HashSet from the elements of the slice
-    set.len() != vec.len() // Check if the lengths are different (indicating duplicates)
-}
-
-
-pub fn perft(sfen: &str, depth: u32) -> u64 {
-    let positions = sfen::sfen_parse(sfen); // Parse the SFEN string into a list of positions
-    let mut pos = sfen::generate_pos(positions); // Generate a "partial position" from the positions
-    pos.side_to_move_set(sfen::get_color(sfen));
-
-    if depth == 0 {
-        return 1; // End of search, return 1 as a leaf node
-    }
-
-    let next_moves = all_legal_moves_partial(&pos); // Get all possible moves for the current position
-    let mut node_count = 0; // Counter for the number of nodes
-
-    for move_item in next_moves {
-        let mut temp_pos = pos.clone(); // Create a temporary copy of the position
-        temp_pos.make_move(move_item); // Make the move on the temporary position
-        let sfen = temp_pos.to_sfen_owned(); // Convert the updated position to an SFEN string
-
-        let child_count = perft(&sfen, depth - 1); // Recursively calculate child nodes count
-        node_count += child_count; // Add child count to the node count
-    }
-
-    node_count // Return the total node count
-}
-
-
-pub fn single_search(sfen: &str) -> (Vec<String>, Vec<Move>) {
-    
-    let positions = sfen::sfen_parse(sfen);
-    let mut pos = sfen::generate_pos(positions);
-    pos.side_to_move_set(sfen::get_color(sfen));
-    let next_moves = all_legal_moves_partial(&pos); 
-    let mut sfen_list = Vec::new();
-    let mut move_list = Vec::new();
-    
-    for move_item in next_moves {
-        move_list.push(move_item);
-        let mut temp_pos = pos.clone();
-        temp_pos.make_move(move_item);
-        let sfen = temp_pos.to_sfen_owned();
-        sfen_list.push(sfen);
-    }
-
-    return (sfen_list, move_list);
-}
-
 
 pub struct DepthFirstIter<'a> {
     stack: VecDeque<&'a GameTree>,
@@ -165,3 +89,84 @@ pub fn treesearch(sfen: &str, depth: u32, current_depth: u32) -> GameTree {
 
     game_tree
 }
+
+
+pub fn single_search(sfen: &str) -> (Vec<String>, Vec<Move>) {
+    
+    let positions = sfen::sfen_parse(sfen);
+    let mut pos = sfen::generate_pos(positions);
+    pos.side_to_move_set(sfen::get_color(sfen));
+    let next_moves = all_legal_moves_partial(&pos); 
+    let mut sfen_list = Vec::new();
+    let mut move_list = Vec::new();
+    
+    for move_item in next_moves {
+        move_list.push(move_item);
+        let mut temp_pos = pos.clone();
+        temp_pos.make_move(move_item);
+        let sfen = temp_pos.to_sfen_owned();
+        sfen_list.push(sfen);
+    }
+
+    return (sfen_list, move_list);
+}
+
+
+pub fn search(sfen: &str, depth: u32, current_depth: u32) -> Vec<String> {
+    let positions = sfen::sfen_parse(sfen);// creates list of board squares and the pieces on them (if there are any)
+    let mut pos = sfen::generate_pos(positions); // creates a "partial position" out of it
+    pos.side_to_move_set(sfen::get_color(sfen));
+
+    if current_depth == depth {
+        return vec![sfen.to_string()]; // Return the SFEN string as a single-element vector
+    }
+
+    let next_moves = all_legal_moves_partial(&pos);
+    let mut sfen_list = Vec::new();
+
+    for move_item in next_moves {
+        let mut temp_pos = pos.clone();
+        temp_pos.make_move(move_item);
+        let sfen = temp_pos.to_sfen_owned();
+
+        let deeper_moves = search(&sfen, depth, current_depth + 1);
+        sfen_list.extend(deeper_moves);
+    }
+
+    sfen_list
+}
+
+
+pub fn has_duplicates<T: std::cmp::Eq + std::hash::Hash>(vec: &[T]) -> bool {
+    let set: HashSet<_> = vec.iter().collect(); // Create a HashSet from the elements of the slice
+    set.len() != vec.len() // Check if the lengths are different (indicating duplicates)
+}
+
+
+pub fn perft(sfen: &str, depth: u32) -> u64 {
+    let positions = sfen::sfen_parse(sfen); // Parse the SFEN string into a list of positions
+    let mut pos = sfen::generate_pos(positions); // Generate a "partial position" from the positions
+    pos.side_to_move_set(sfen::get_color(sfen));
+
+    if depth == 0 {
+        return 1; // End of search, return 1 as a leaf node
+    }
+
+    let next_moves = all_legal_moves_partial(&pos); // Get all possible moves for the current position
+    let mut node_count = 0; // Counter for the number of nodes
+
+    for move_item in next_moves {
+        let mut temp_pos = pos.clone(); // Create a temporary copy of the position
+        temp_pos.make_move(move_item); // Make the move on the temporary position
+        let sfen = temp_pos.to_sfen_owned(); // Convert the updated position to an SFEN string
+
+        let child_count = perft(&sfen, depth - 1); // Recursively calculate child nodes count
+        node_count += child_count; // Add child count to the node count
+    }
+
+    node_count // Return the total node count
+}
+
+
+
+
