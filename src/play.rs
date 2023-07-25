0875489cd4ab13dad2354ea7d0e9350786e2ec5f
russellkosovsky <russellkosovsky@gmail.com>
2023-07-25 16:26:41 -0400

    coming along

--- src/play.rs
+++ src/play.rs
@@ -9,10 +9,14 @@ use shogi_legality_lite::{normal_from_candidates, is_legal_partial_lite, all_leg
 use shogi_core::{PartialPosition, Square, Piece, Color, Move, PieceKind};
 
 
-fn validate_user_move(user_move: &str) -> bool {
+fn validate_user_move(user_input: &str) -> bool {
     // add validation logic
     // check that the input is formatted to represent a valid move in accordance with shogi_core.
-    true
+    if user_input.len() == 10 {
+        true
+    } else {
+        false
+    }
 }
 
 
@@ -25,6 +29,7 @@ pub fn play() {
     println!(" | in this game, squares are represented by their rank and file (rank, file)");
     println!(" | this means that your king would be in square: 'I,5'");
     println!(" | ranks are always a capital letter from A-I and files an integer from 1-9 ");
+    println!(" | please enter your moves in the exact format as follows: 'G,9 to F,9'");
     println!(" | ");
     println!(" |-------------------------------------------------------------------------|");
     println!(" | ");
@@ -34,27 +39,46 @@ pub fn play() {
     //println!("sfen: {:?}", sfen);
     view::display_sfen(&sfen);
 
-    println!(" | ");
-    println!(" |-----------------------------Enter Your Move-----------------------------|");
-    //println!(" | ");
-
-    let mut user_move = String::new();
-    match io::stdin().read_line(&mut user_move) {
-        Ok(_) => {
-            user_move = user_move.trim().to_string(); // Trim the input
-            if validate_user_move(&user_move) { // Validate the input
-                println!(" | Your Move: {:?}", user_move);
-            } else {
-                println!(" | Invalid move. Please enter a valid move in the format 'I,5'.");
-            }
-        },
-        Err(e) => {
-            println!(" | There was an error reading your input: {}. Please try again.", e);
-        },
+    let mut input = String::new();
+    loop {
+        println!(" | ");
+        println!(" |-----------------------------Enter Your Move-----------------------------|");
+        println!(" | ");
+        
+        input.clear();
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => {
+                input = input.trim().to_string(); // Trim the input
+                if validate_user_move(&input) { // Validate the input
+                    break;
+                } else {
+                    println!(" | ");
+                    println!(" | Please enter a valid move in the format: 'I,5 to H,5'.");
+                }
+            },
+            Err(e) => {
+                println!(" | ");
+                println!(" | There was an error reading your input: {}. Please try again.", e);
+                println!(" | ");
+            },
     
+        }
     }
 
-    println!(" | {:?}", user_move);
+    let moves: Vec<&str> = input.split(" to ").collect();
+    
+    let (from_sqr, to_sqr) = (moves[0], moves[1]);
 
+    println!(" | moving piece from square {} to square {}", from_sqr, to_sqr);
+
+    println!("from_sqr: {:?}", from_sqr);
+    println!("to_sqr: {:?}", to_sqr);
+    
+    //println!(" | {:?}", input);
+    //let shii = input.split(",");
+    //for chr in shii {
+    //    println!(" | {:?}", chr);
+
+    //}
 }
 
